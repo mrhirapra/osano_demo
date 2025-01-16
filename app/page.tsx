8244a3c195386164f547caa35c42ef4a8718fbf7
Kaushik Hirapara <84130774+mrhirapra@users.noreply.github.com>
2025-01-16 17:52:13 +0530

    push

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="h-screen flex items-center justify-center">
+        <p className="font-serif text-3xl capitalize">osano demo</p>
+    </div>
+  );
+}
---
+++ app/privacy/page.tsx
@@ -0,0 +1,105 @@
+import React from 'react';
+
+export default function PrivacyPolicy() {
+  return (
+    <div className="p-6 bg-gray-50 text-gray-800">
+      <h1 className="text-2xl font-bold mb-4">Privacy Policy</h1>
+
+      <p className="mb-4">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus quod consectetur soluta quis, laudantium necessitatibus ab non.
+      </p>
+
+      <p className="mb-4">
+        Soluta quisquam, ad officiis nesciunt blanditiis iure qui exercitationem atque impedit aut officia iste perspiciatis maxime, dolore nobis facere non a at tempora quod ea labore sequi illo?
+      </p>
+
+      <p className="mb-4">
+        Ut esse laborum animi quis id eius impedit amet voluptates rem aut iste sint voluptas culpa molestias, ipsam odit maxime magnam explicabo, commodi dicta sit a aliquam, totam enim.
+      </p>
+
+      <p className="mb-4">
+        Itaque, rerum? Numquam sed praesentium facilis dolore tenetur sint est et voluptatibus repellat molestiae.
+      </p>
+
+      <p className="mb-4">
+        Eaque quam omnis voluptatibus, labore assumenda temporibus exercitationem minus quibusdam magnam necessitatibus dolorem praesentium odio harum neque corrupti voluptate!
+      </p>
+
+      <p className="mb-4">
+        Fugit ea blanditiis iusto consequatur dignissimos fugiat esse eos quaerat possimus quisquam et omnis rem doloremque, laboriosam expedita, placeat ad nesciunt necessitatibus deleniti enim voluptas architecto cum?
+      </p>
+
+      <p className="mb-4">
+        Labore harum repellendus reprehenderit est fuga ex, error, deleniti placeat iusto soluta ratione voluptates ipsam totam inventore alias excepturi dicta cum?
+      </p>
+
+      <p className="mb-4">
+        In dolores aliquam earum impedit reiciendis alias et corporis nobis ipsa rem? Consectetur expedita ex est aperiam laudantium numquam sunt exercitationem, quas repellendus.
+      </p>
+
+      <p className="mb-4">
+        Dicta, temporibus fuga dignissimos ducimus tempore a? Ut illo debitis reiciendis sunt eius voluptatem odit consectetur voluptate facilis.
+      </p>
+
+      <p className="mb-4">
+        Obcaecati cumque et esse blanditiis facere qui ipsam dignissimos voluptatibus eligendi asperiores illo deleniti quo, labore modi sint harum inventore accusantium sit ducimus at necessitatibus!
+      </p>
+
+      <p className="mb-4">
+        Rem minus, quo aspernatur alias illum labore fuga vero doloribus, provident sit a voluptatibus necessitatibus eligendi quam?
+      </p>
+
+      <p className="mb-4">
+        Excepturi reprehenderit magni ratione laborum et placeat soluta amet. In, est unde. Accusamus minima illo voluptatum dolore molestiae.
+      </p>
+
+      <p className="mb-4">
+        Laborum quis magni corrupti ad expedita eaque, nostrum pariatur omnis temporibus maiores et recusandae libero in odit placeat voluptates eos iusto numquam explicabo excepturi tenetur, sint labore dignissimos dicta!
+      </p>
+
+      <p className="mb-4">
+        Illo nihil, debitis dolore ab, vitae minus reprehenderit magnam soluta voluptatum fugiat quo eveniet eius voluptatem?
+      </p>
+
+      <p className="mb-4">
+        Ex earum aspernatur voluptates neque nesciunt placeat quae, alias commodi at ea voluptatem adipisci et blanditiis hic corrupti amet a repellat nulla, ipsum culpa laboriosam accusamus harum molestias modi?
+      </p>
+
+      <p className="mb-4">
+        Delectus dignissimos facere excepturi sequi similique fugiat expedita dolores nostrum hic quasi velit, magnam dolorem aliquid nihil ipsum sapiente maxime!
+      </p>
+
+      <p className="mb-4">
+        Minima, similique. Similique dolorem odio cum, sunt dolore odit quas in, quasi ipsam recusandae saepe doloremque, repudiandae dolor voluptatibus. Sunt, aliquam.
+      </p>
+
+      <p className="mb-4">
+        Laborum corporis id suscipit dolore necessitatibus eligendi alias pariatur quibusdam, obcaecati qui magni ratione consequatur sequi eius a voluptatibus dignissimos quae laboriosam dolor officiis doloremque!
+      </p>
+
+      <p className="mb-4">
+        Animi sunt eum eligendi quos aliquid quidem excepturi delectus, eius dolores. Tenetur rerum voluptatibus quis illum iste cum officia autem.
+      </p>
+
+      <p className="mb-4">
+        Nobis voluptatum odit enim, ipsa, vitae at corrupti rerum reiciendis nulla assumenda dolore ipsam? Sint, aliquid animi.
+      </p>
+
+      <p className="mb-4">
+        Unde numquam beatae recusandae, neque aut ipsa officia harum praesentium, quisquam dolorem accusantium nihil reiciendis soluta quos iusto.
+      </p>
+
+      <p className="mb-4">
+        Nisi harum, ducimus omnis architecto atque unde fugit velit ea, voluptatem adipisci maiores est libero sint cupiditate minus ratione nostrum non dolorum.
+      </p>
+
+      <p className="mb-4">
+        Nisi odio inventore modi numquam omnis doloremque aut maxime at, laudantium nam corporis ipsam assumenda non corrupti voluptatibus asperiores error fugit distinctio officia provident.
+      </p>
+
+      <p className="mb-4">
+        Ipsam, praesentium id.
+      </p>
+    </div>
+  );
+}
